aedfa71884f149778225458ccfff916cab98fad3
Zhang WJ <lemon.zhang@servasoft.com>
2019-09-26 15:50:34 +0800

    修改eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  extends: "airbnb-base",
+  plugins: ["prettier"],
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    allowImportExportEverywhere: false,
+    codeFrame: true
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  globals: {
+    $: true,
+    echarts: true,
+    twaver: "readonly",
+    mono: "readonly"
+  },
+  rules: {
+    "no-console": 0,
+    "no-unused-vars": [
+      "error",
+      {
+        args: "none"
+      }
+    ],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    // disallow parameter object manipulation except for specific exclusions
+    "no-param-reassign": [
+      "off",
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          "state", // for vuex state
+          "acc", // for reduce accumulators
+          "e" // for e.returnvalue
+        ]
+      }
+    ]
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        moduleDirectory: ["node_modules", "docs/js/libs/"]
+      }
+    }
+  }
+};
